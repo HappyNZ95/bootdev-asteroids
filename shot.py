04183820d421cf40c901e7ade3a65956882859a0
HappyNZ95 <htewini@gmail.com>
2025-03-30 15:17:30 +1300

    added player shoot method and shot class

--- shot.py
+++ shot.py
@@ -1,9 +1,13 @@
 import pygame
 from circleshape import *
-class Asteroid(CircleShape):
+
+class Shot(CircleShape):
     def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
-        print("I'm an asteroid woohoo!")
+        super().__init__(x,y, radius)
+        print("I'm a shot yippee!")
+
+        self.x = x
+        self.y = y
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
